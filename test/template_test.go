f1bebafecad6f8b27e1102093f3e236d56e33d34
Ken <563068223@qq.com>
2024-07-27 18:23:41 +0800

    add: 新增创建模板的单元测试

--- test/template_test.go
+++ test/template_test.go
@@ -13,3 +13,13 @@ func TestGetAllTemplates(t *testing.T) {
 		fmt.Println(item.Name)
 	}
 }
+
+func TestCreateTemplate(t *testing.T) {
+	name := ""
+	template := template2.NewTemplate()
+	isSuccess, err := template.CreateTemplate(name)
+	if err != nil {
+		t.Fatalf("让我看看到底是什么是垃圾报错： %v", err)
+	}
+	t.Logf("创建模板成功 你太酷了, %v", isSuccess)
+}

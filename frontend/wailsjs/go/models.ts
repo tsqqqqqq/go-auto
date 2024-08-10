2ab3580970c15cad1a2f3f700802a74cd9763985
Ken <563068223@qq.com>
2024-08-10 20:10:04 +0800

    add：页面新增窗口选项

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -15,3 +15,20 @@ export namespace template {
 
 }
 
+export namespace window {
+	
+	export class Window {
+	
+	
+	    static createFrom(source: any = {}) {
+	        return new Window(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	
+	    }
+	}
+
+}
+
